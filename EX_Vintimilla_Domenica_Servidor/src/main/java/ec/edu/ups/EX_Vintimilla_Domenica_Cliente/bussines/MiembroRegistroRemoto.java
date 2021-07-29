4609d48d3f5b05557640b6e9469ecc2c805ac9be
ShootDomy <ShootDomy@DESKTOP-9CU946S>
2021-07-29 14:43:34 -0500

    Implemetar metodos

--- EX_Vintimilla_Domenica_Servidor/src/main/java/ec/edu/ups/EX_Vintimilla_Domenica_Cliente/bussines/MiembroRegistroRemoto.java
+++ EX_Vintimilla_Domenica_Servidor/src/main/java/ec/edu/ups/EX_Vintimilla_Domenica_Cliente/bussines/MiembroRegistroRemoto.java
@@ -1,13 +1,22 @@
 package ec.edu.ups.EX_Vintimilla_Domenica_Cliente.bussines;
 
+import java.util.List;
+
 import javax.ejb.Remote;
 
+import ec.edu.ups.EX_Vintimilla_Domenica_Cliente.modelo.Carrera;
 import ec.edu.ups.EX_Vintimilla_Domenica_Cliente.modelo.Universidad;
 
 
 @Remote
 public interface MiembroRegistroRemoto {
 
-	public void insertaUni(Universidad universidad);
+public void guardarCarrera(Carrera carrera);
+	
+	public Universidad getUni(int codigo);
+	
+	public List<Carrera> getCarreras();
+	
+	public List<Carrera> getCars(int codigo);
 	
 }
